abe6a1b73030f25c6ec28a18a13220cec7a74db0
gab759 <150464325+gab759@users.noreply.github.com>
2024-10-07 16:43:11 -0500

    Scripts Set Up

---
+++ Assets/Scripts/ScriptableObjects/ColorShapeData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Color Shape Data", menuName = "Scriptable Objects/Game 1/ColorShapeData")]
+public class ColorShapeData : ScriptableObject
+{
+    
+}
